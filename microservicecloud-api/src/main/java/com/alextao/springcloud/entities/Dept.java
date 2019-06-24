4dd5192a9ea6318467f8bc859324e1636cca39d0
Tao Siyu <alextao52100313@gmail.com>
2019-06-25 00:26:57 +0800

    Initial commit

---
+++ microservicecloud-api/src/main/java/com/alextao/springcloud/entities/Dept.java
@@ -0,0 +1,17 @@
+package com.alextao.springcloud.entities;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@SuppressWarnings("serial")
+@NoArgsConstructor
+@Data
+@Accessors(chain=true)
+public class Dept implements Serializable{
+	private Long deptno;
+	private String dname;
+	private String db_source;
+}
---
+++ microservicecloud-api/src/test/java/com/alextao/springcloud/test/Test.java
@@ -0,0 +1,5 @@
+package com.alextao.springcloud.test;
+
+public class Test {
+
+}
