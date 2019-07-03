5dbf6747ba93220d0861f20b53d3d7ec26ba7702
Tao Siyu <alextao52100313@gmail.com>
2019-07-03 20:25:46 +0800

    2019/7/3
    2019.7.3

---
+++ micorservicecloud-consumer-dept-80/src/main/java/com/alextao/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,16 @@
+package com.alextao.springcloud.cfgbeans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean {// spring中使用applicationContext.xml作为配置文件，在springboot中没有该文件而是直接使用annotation版本的配置即@Configuration
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+		//等价于注解中的 applicationContext.xml == ConfigBean(@Configuration)
+		//<bean id="RestTemplate" class="com.alextao.springcloud.cfgbeans.getRestTemplate">
+	}
+}
